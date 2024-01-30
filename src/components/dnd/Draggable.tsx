15c61bac38f1e6287675973ab7a11b758d53820e
ravindranag <ravindranag52@gmail.com>
2024-01-31 03:02:03 +0530

    chore: drag cursor

--- src/components/dnd/Draggable.tsx
+++ src/components/dnd/Draggable.tsx
@@ -2,30 +2,33 @@ import { UseDraggableArguments, useDraggable } from "@dnd-kit/core";
 import { CSSProperties, PropsWithChildren } from "react";
 
 type DraggableItemProps = {
-  id: string;
-  data?: UseDraggableArguments['data']
+	id: string;
+	data?: UseDraggableArguments['data']
 }
 
 const DraggableItem = ({ id, children, data }: PropsWithChildren<DraggableItemProps>) => {
-  const { setNodeRef, attributes, listeners, transform } = useDraggable({
-    id,
-    data,
-  })
+	const { setNodeRef, attributes, listeners, transform, isDragging } = useDraggable({
+		id,
+		data,
+	})
 
-  const style: CSSProperties | undefined = transform ? {
-    transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`
-  } : undefined
+	const style: CSSProperties | undefined = transform ? {
+		transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`
+	} : undefined
 
-  return (
-    <div
-      ref={setNodeRef}
-      style={style}
-      {...listeners}
-      {...attributes}
-    >
-      {children}
-    </div>
-  )
+	return (
+		<div
+			ref={setNodeRef}
+			style={{
+				...style,
+				cursor: isDragging ? 'grabbing' : 'grab'
+			}}
+			{...listeners}
+			{...attributes}
+		>
+			{children}
+		</div>
+	)
 }
 
 export default DraggableItem
